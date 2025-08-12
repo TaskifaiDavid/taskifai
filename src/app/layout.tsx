7de4ad0676cb1b25b400e5523cc85a8327040a4c
TaskifaiDavid <david@taskifai.com>
2025-08-12 15:26:27 +0200

    Add LinkedIn Insight Tag for conversion tracking
    - Implement LinkedIn partner ID 8648577 in layout.tsx
    - Add async script loading for LinkedIn analytics
    - Include noscript fallback pixel for non-JS users
    - GDPR compliant implementation with existing cookie consent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -183,6 +183,46 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbSchema) }}
         />
+        
+        {/* LinkedIn Insight Tag */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              _linkedin_partner_id = "8648577";
+              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            `
+          }}
+        />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(l) {
+                if (!l){
+                  window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                  window.lintrk.q=[]
+                }
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";
+                b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+              })(window.lintrk);
+            `
+          }}
+        />
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}} 
+            alt="" 
+            src="https://px.ads.linkedin.com/collect/?pid=8648577&fmt=gif" 
+          />
+        </noscript>
       </head>
       <body className="font-sans">
         <MobilePerformanceProvider>
